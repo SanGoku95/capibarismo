66400024784f75abb13284342889c4eefb9d168a
Italo sayan <italosayan@gmail.com>
2025-10-07 09:49:01 +0200

    cambiar título

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -37,7 +37,7 @@ export function HomePage() {
             CAPIBARISMO
           </h1>
           <p className="mt-3 text-[0.85rem] sm:text-base md:text-lg max-w-2xl mx-auto text-foreground/90 font-sans px-2 leading-relaxed">
-            Compara candidatos y entiende sus posturas clave para las elecciones 2026.
+            Tu Dibujito Observador
           </p>
         </div>
         
